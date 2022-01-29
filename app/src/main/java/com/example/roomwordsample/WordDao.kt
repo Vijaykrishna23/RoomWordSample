e3d08d7baf3270fa3dbddc2f0989bda2b9d1f4b5
Vijaykrishna23 <vk1500@srmist.edu.in>
2022-01-29 13:24:01 +0530

    added more data

--- app/src/main/java/com/example/roomwordsample/WordDao.kt
+++ app/src/main/java/com/example/roomwordsample/WordDao.kt
@@ -1,9 +1,6 @@
 package com.example.roomwordsample
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,8 +10,8 @@ interface WordDao {
     fun getAlphabetizedWords(): Flow<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(word: Word)
+    fun insert(word: Word)
 
     @Query("DELETE FROM word_table")
-    suspend fun deleteAll()
+    fun deleteAll()
 }
